fa6358645b5cb68141790c54055b57bab2673589
Brant Brown <icpdeland@gmail.com>
2022-05-23 09:48:39 +0000

    Init:migrate

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+# Local Cache
+
+[![publish Node.js Package](https://github.com/Deland-Labs/localCache/actions/workflows/publish.yml/badge.svg)][1]
+
+## Usage
+
+```shell
+npm install @deland-labs/local-cache
+```
+
+## Document
+
+```shell
+npm start
+```
+
+## Publish
+
+```shell
+git tag vX.Y.Z HEAD  # Tag names started with "v" will trig CI/CD pipeline
+
+git push origin master --tags
+```
+
+[1]: https://github.com/Deland-Labs/localCache/actions/workflows/publish.yml
+[2]: https://github.com/settings/tokens
---
+++ src/ILocalCache.ts
@@ -0,0 +1,35 @@
+export interface ILocalCache {
+  getCurrentBucket: () => string;
+  setCurrentBucket: (bucket: string) => Promise<void>;
+  set: (key: string, value: any, ttl?: number) => Promise<void>;
+  get: (key: string) => Promise<any>;
+  delete: (key: string) => Promise<void>;
+  flush: (expired: boolean) => Promise<void>;
+  flushBucket: (expired: boolean, bucket?: string) => Promise<void>;
+  buckets: () => Promise<Array<string>>;
+}
+
+export const jsonStringify = (value: any) => {
+  try {
+    return JSON.stringify(value, (key, value) =>
+      typeof value === 'bigint' ? value.toString() + 'n' : value
+    );
+  } catch (e) {
+    console.log(`Couldn't convert value to JSON, e: ${e}`);
+  }
+};
+export const jsonParse = (json: any) => {
+  try {
+    return JSON.parse(json, (key, v) => {
+      if (typeof v === 'string' && /^\d+n$/.test(v)) {
+        return BigInt(v.substr(0, v.length - 1));
+      }
+      return v;
+    });
+  } catch (e) {
+    console.log(`Couldn't parse value, e: ${e}`);
+  }
+};
+
+export const isExpired = (expiryDateInMilliseconds: number) =>
+  Date.now() >= expiryDateInMilliseconds;
